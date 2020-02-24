412435af260499e804eb61c0669c9bd1414b03c3
rajeevdodda <rajeevdodda@gmail.com>
2020-02-25 01:28:05 +0530

    Circular queues

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -1,79 +1,60 @@
 class Node:
 
-    def __init__(self, data=None):
-        self.data = data
+    def __init__(self, element):
+        self._element = element
         self.next = None
 
 
 class LinkedList:
 
     def __init__(self):
-        self.head = None
+        self._head = None
+        self._size = 0
 
-    def search(self, k):
-        """ search takes O(n) times in worst case """
-        current_node = self.head
-        while current_node is not None and current_node.data != k:
-            current_node = current_node.next
-
-        if current_node:
-            return current_node.data
+    def append(self, element):
+        if self._head is None:
+            self._head = Node(element)
         else:
-            return current_node
+            next_element = Node(element)
+            next_element.next = self._head
+            self._head = next_element
+        self._size += 1
 
-    def insert(self, new_node):
-        new_node.next = self.head
-        self.head = new_node
-        return True
+    def search(self, element):
+        next_element = self._head
+        while next_element is not None and next_element._element != element:
+            next_element = next_element.next
+        return next_element
 
-    def insert_last(self, new_node):
-        """ insertion takes O(1) time. """
-        if self.head is None:
-            self.head = new_node
+    def delete(self, element):
+        next_element = self._head
+
+        # check if head exists
+        if next_element is None:
+            return
+        # check if element is head or not
+        if next_element._element == element:
+            self._head = next_element.next
+            self._size -= 1
+            return
+
+        while next_element is not None and next_element._element != element:
+            prev_element = next_element
+            next_element = next_element.next
+        if next_element is None:
+            return "Element not found"
         else:
-            last_node = self.head
-            while True:
-                if last_node.next is None:
-                    break
-                last_node = last_node.next
-            last_node.next = new_node
-
-    def get_list(self):
-        current_node = self.head
-        elements = []
-        if current_node is None:
-            return elements
-        while True:
-            elements.append(current_node.data)
-            if current_node.next is None:
-                break
-
-            current_node = current_node.next
-        return elements
-
-    def delete(self, x):
-        if self.head is None:
-            return None
-
-        if self.head.data == x:
-            self.head = self.head.next
-            return True
-
-        current_node = self.head
-        while current_node is not None and current_node.data != x:
-            pre_node = current_node
-            current_node = current_node.next
-        if current_node:
-            pre_node.next = current_node.next
-            return True
-        else:
-            return False
+            prev_element.next = next_element.next
+            self._size -= 1
+            return "Element deleted"
 
 
-linked_list_instance = LinkedList()
 
-linked_list_instance.insert(Node(1))
-linked_list_instance.insert(Node(2))
-linked_list_instance.insert(Node(3))
-linked_list_instance.insert_last(Node(4))
-print(linked_list_instance.get_list())
+l = LinkedList()
+print(l.delete(1))
+print(l._size)
+print(l.append(1))
+print(l._size)
+print(l.search(1))
+print(l.delete(1))
+print(l._size)
